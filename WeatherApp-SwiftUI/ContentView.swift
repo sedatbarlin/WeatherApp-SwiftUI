62d28c5ba53e1efd6e0474c6fb107eaaa9aa438a
Sedat Barlin <barlinsedat@gmail.com>
2024-06-10 17:09:05 +0300

    working on it

--- WeatherApp-SwiftUI/ContentView.swift
+++ WeatherApp-SwiftUI/ContentView.swift
@@ -13,8 +13,9 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Sedat Barlin")
         }
+        
         .padding()
     }
 }
